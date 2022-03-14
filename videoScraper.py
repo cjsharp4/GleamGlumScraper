685e3d29ad8697254341f860c68231f1a53e130d
cjsharp4 <65328908+cjsharp4@users.noreply.github.com>
2022-03-13 21:38:14 -0700

    Can now iterate through a list of videos

--- videoScraper.py
+++ videoScraper.py
@@ -5,6 +5,24 @@ from moviepy.editor import * #pip install moviepy
 import cv2 #pip install opencv-python
 import os 
 import pathlib #pip install pathlib
+from csv import reader
+
+# open file in read mode
+with open('trending_links.csv', 'r') as read_obj:
+    # pass the file object to reader() to get the reader object
+    csv_reader = reader(read_obj)
+    # Iterate over each row in the csv using reader object
+    video_id_list = []
+    video_link_list = []
+    for row in csv_reader:
+        #iterate through first row 
+        for i in range(0,len(row)):
+        	#check if row is empty 
+        	if row[i]:
+        		#get video link 
+        		video_link_list.append(row[i])
+        		#get video id
+        		video_id_list.append(row[i].split("v=",1)[1]) 
 
 
 ##TODO require coords to be certain size so that face is big enough to see in video
@@ -69,72 +87,87 @@ def saveClip(clip_name):
 """
 example video link:
 https://www.youtube.com/watch?v=7iONU9LxeV8
-
 this is the video id:
 7iONU9LxeV8
 """
 
-video_link = 'https://www.youtube.com/watch?v=7iONU9LxeV8'  
-root = YouTubeTranscriptApi.get_transcript('7iONU9LxeV8') 
+#video_link = 'https://www.youtube.com/watch?v=7iONU9LxeV8'  
+#root = YouTubeTranscriptApi.get_transcript('7iONU9LxeV8') 
 
 #for key in root:
 #	print(key['text'])
 	
-ee_uh_words = ["glum" , "gleam"] #list of words that we are looking to find in the video
+ee_uh_words = ["glum" , "gleam", "hello"] #list of words that we are looking to find in the video
 
-word_found = False
-which_word = "###"
-clip_number = 1
-for child in root:	
-	transcript_section = child['text'].split()
 
-	#begin downloading and clipping a video if a word from the list is found in the video's transcript
-	if(any(word in transcript_section for word in ee_uh_words)):
+for link in range(0,len(video_link_list)):
+
+	video_link = video_link_list[link]  
+	
+	try:
+		root = YouTubeTranscriptApi.get_transcript(video_id_list[link])
+	except:
+		root = "fail"
+
+	if(root != "fail"):
+		word_found = False
+		which_word = "###"
+		clip_number = 1
+		for child in root:	
+			transcript_section = child['text'].split()
+
+			#begin downloading and clipping a video if a word from the list is found in the video's transcript
+			if(any(word in transcript_section for word in ee_uh_words)):
+				
+				print("target word found")
+
+				#get which word was said in the clip
+				for word in ee_uh_words:
+					if(word in transcript_section):
+						which_word = word
+
+				#for the first instance of a word being found, download the current video
+				if(word_found == False):
+					yt = YouTube(video_link)
+					video_title = yt.title
+					video_filename = video_title + ".mp4"
+					#yt.streams.first().download()
+					download_video = yt.streams.filter(progressive = True, file_extension = "mp4").first().download(filename=video_filename)
+					word_found = True
+
+				#find the start and ending times of each transcript portion with a target word
+				start = int(float(child['start']))
+				end = int(float(child['duration'])) + start + 1
+				clip = VideoFileClip(video_filename).subclip(start, end)
+
+				#name file with video's title, the target word found, and number of clip within an individual video 
+				clip_name = video_title + "_" + which_word + "_clip" + str(clip_number) + ".mp4"
+				clip_number += 1
+
+				#write file that will contain video and audio
+				clip.write_videofile(clip_name, temp_audiofile='temp-audio.m4a', remove_temp=True, codec="libx264", audio_codec="aac")
+				#print(unescape(child.text), int(float(child.attrib['start'])), "\n")
+
+				clip.close()
+
+				#if a face is detected for at least 80% of the video
+				if(faceDetect(clip_name)):
+					saveClip(clip_name)
+				else:
+					removeFile = os.getcwd() + '/' + clip_name
+					if(os.path.isfile(removeFile)):
+						os.remove(removeFile)
+
+
 		
-		#get which word was said in the clip
-		for word in ee_uh_words:
-			if(word in transcript_section):
-				which_word = word
-
-		#for the first instance of a word being found, download the current video
-		if(word_found == False):
-			yt = YouTube(video_link)
-			video_title = yt.title
-			video_filename = video_title + ".mp4"
-			#yt.streams.first().download()
-			download_video = yt.streams.filter(progressive = True, file_extension = "mp4").first().download(filename=video_filename)
-			word_found = True
-
-		#find the start and ending times of each transcript portion with a target word
-		start = int(float(child['start']))
-		end = int(float(child['duration'])) + start + 1
-		clip = VideoFileClip(video_filename).subclip(start, end)
-
-		#name file with video's title, the target word found, and number of clip within an individual video 
-		clip_name = video_title + "_" + which_word + "_clip" + str(clip_number) + ".mp4"
-		clip_number += 1
-
-		#write file that will contain video and audio
-		clip.write_videofile(clip_name, temp_audiofile='temp-audio.m4a', remove_temp=True, codec="libx264", audio_codec="aac")
-		#print(unescape(child.text), int(float(child.attrib['start'])), "\n")
-
-		clip.close()
-
-		#if a face is detected for at least 80% of the video
-		if(faceDetect(clip_name)):
-			saveClip(clip_name)
+		#remove full length downloaded video
+		try:
+			removeFile = os.getcwd() + '/' + video_filename
+		except:
+			print("video did not contain any of the target words")
+			video_filename = None
 		else:
-			removeFile = os.getcwd() + '/' + clip_name
 			if(os.path.isfile(removeFile)):
 				os.remove(removeFile)
-
-
-
-#remove full length downloaded video
-try:
-	removeFile = os.getcwd() + '/' + video_filename
-	if(os.path.isfile(removeFile)):
-		os.remove(removeFile)
-except NameError:
-	print("video did not contain any of the target words")
-	video_filename = None
+			
+		
